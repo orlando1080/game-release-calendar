51b0497fa1f8555648082ed7c82f72bd20f343bd
orlando1080 <lewislsewell@gmail.com>
2023-05-07 21:52:14 +0100

    Changed to classes

--- game_release_calendar.py
+++ game_release_calendar.py
@@ -1,16 +1,26 @@
 import re
+
 import urllib.request
 import bs4 as bs
 
-url = 'https://gamerant.com/ps5-game-release-dates/'
 
-page = urllib.request.urlopen(url).read()
-soup = bs.BeautifulSoup(page, 'lxml')
-ul_tags = soup.find_all('ul')
-li_tags = []
-for ul_tag in ul_tags:
-    for li_tag in ul_tag.find_all('li'):
-        if re.search(r'(?:January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}',
-                     li_tag.get_text()):
-            li_tags.append(li_tag.text.strip())
-print(li_tags)
+class Scaper:
+    def __init__(self, site):
+        """Create an instance for scaper class."""
+        self.site = site
+        self.li_tags = []
+
+    def scaper(self):
+        """Scraps data from site."""
+        url = self.site
+        page = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(page, 'lxml')
+        ul_tags = soup.find_all('ul')
+        self.li_tags = [li_tag.text.strip() for ul_tag in ul_tags for li_tag in ul_tag.find_all('li') if
+                        re.match(
+                            r'^(January|February|March|April|May|June|July|August|September|October|November|December)',
+                            li_tag.get_text().strip())]
+
+
+ps5 = Scaper('https://gamerant.com/ps5-game-release-dates/')
+ps5.scaper()
