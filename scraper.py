d59bfe7d457bb9c1e3f32b9e91f94a65abd13187
orlando1080 <lewislsewell@gmail.com>
2023-05-10 21:57:28 +0100

    change name to scraper.py

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import re
+
+import urllib.request
+import bs4 as bs
+
+
+class Scraper:
+    def __init__(self, site):
+        """Create an instance for scaper class."""
+        self.site = site
+        self.li_tags = []
+
+    def scraper(self):
+        """Scraps data from site."""
+        url = self.site
+        page = urllib.request.urlopen(url).read()
+        soup = bs.BeautifulSoup(page, 'lxml')
+        ul_tags = soup.find_all('ul')
+        self.li_tags = [li_tag.text.strip() for ul_tag in ul_tags for li_tag in ul_tag.find_all('li') if
+                        re.match(
+                            r'^(January|February|March|April|May|June|July|August|September|October|November|December)',
+                            li_tag.get_text().strip())]
+
